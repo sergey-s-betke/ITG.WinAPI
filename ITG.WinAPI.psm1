d07a4f81821a2e77e03b1ae32f4abf410bbad6be
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-10-03 14:02:46 +0400

    Первая редакция в GIT
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

---
+++ ITG.WinAPI.psm1
@@ -0,0 +1,38 @@
+﻿Add-Type @" 
+
+using System; 
+using System.Runtime.InteropServices; 
+public class WinAPI { 
+	
+	// http://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
+	[DllImport("user32.dll")] 
+	[return: MarshalAs(UnmanagedType.Bool)] 
+	public
+	static
+	extern
+	bool
+	SetWindowPos(
+		IntPtr hWnd, 
+		IntPtr hWndInsertAfter,
+		int x, int y, 
+		int cx, int cy, 
+		uint uFlags
+	);
+
+	// http://msdn.microsoft.com/en-us/library/windows/desktop/ms633539(v=vs.85).aspx
+	[DllImport("user32.dll")] 
+	[return: MarshalAs(UnmanagedType.Bool)] 
+	public
+	static
+	extern
+	bool
+	SetForegroundWindow(
+		IntPtr hWnd
+	);
+
+} 
+
+"@;
+
+Export-ModuleMember `
+;
