ea16797188d0bd479b45de2834fb3e52502b7eb5
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-10-07 00:26:22 +0400

    + реализовал функцию Get-MIME и протестировал (работает как по данным из файла, так и по расширению имени файла при отсутствии файла)

--- ITG.WinAPI.UrlMon.psm1
+++ ITG.WinAPI.UrlMon.psm1
@@ -3,7 +3,6 @@
         -path $PSScriptRoot `
         -childPath 'ITG.WinAPI.Common' `
     ) `
-    -Force `
 ;
 
 'ITG.WinAPI.UrlMon.cs' `
@@ -17,5 +16,80 @@
 } `
 ;
 
+function Get-MIME {
+	<#
+		.Synopsis
+		    Функция определяет MIME тип файла по его содержимому и расширению имени файла.
+		.Description
+		    Функция определяет MIME тип файла по его содержимому и расширению имени файла
+            (если файл недоступен).
+            Обёртка для API FindMimeFromData.
+        .Link
+            http://msdn.microsoft.com/en-us/library/ms775107(VS.85).aspx
+            http://social.msdn.microsoft.com/Forums/en-US/Vsexpressvcs/thread/d79e76e3-b8c9-4fce-a97d-94ded18ea4dd
+		.Example
+			"logo.jpg" | Get-MIME
+	#>
+    [CmdletBinding(
+    )]
+	
+    param (
+        # путь к файлу, MIME для которого необходимо определить
+        [Parameter(
+			Mandatory=$true,
+			Position=0,
+			ValueFromPipeline=$true
+		)]
+        [System.IO.FileInfo]$Path
+	)
+
+    process {
+        $length = 0;
+        [Byte[]] $buffer = $null;
+        if ( [System.IO.File]::Exists( $Path ) ) {
+            Write-Verbose "Определяем MIME тип файла по его содержимому (файл $($Path.FullName) доступен).";
+            $fs = New-Object System.IO.FileStream (
+                $Path, 
+                [System.IO.FileMode]::Open,
+                [System.IO.FileAccess]::Read,
+                [system.IO.FileShare]::Read,
+                256
+            );
+            try {
+                if ( $fs.Length -ge 256) {
+                    $length = 256;
+                } else {
+                    $length = $fs.Length;
+                };
+                if ( $length ) {
+                    $buffer = New-Object Byte[] (256);
+                    $length = $fs.Read( $buffer, 0, $length );
+                };
+            } finally {
+                $fs.Dispose();
+            };
+        } else {
+            Write-Verbose "Определяем MIME тип файла по расширению его имени (файл $($Path.FullName) недоступен).";
+        };
+        [System.UInt32] $mimeType = 0;
+        $err = [ITG.WinAPI.UrlMon]::FindMimeFromData(
+            0,
+            $Path.FullName, 
+            $buffer, 
+            $length, 
+            $null, 
+            0x00000001 + 0x00000020, 
+            [ref]$mimeType, 
+            0
+        );
+
+        $mimeTypePtr = [System.IntPtr]$mimeType;
+        $mime = [System.Runtime.InteropServices.Marshal]::PtrToStringUni( $mimeTypePtr );
+        [System.Runtime.InteropServices.Marshal]::FreeCoTaskMem( $mimeTypePtr );
+        $mime;
+	}
+}  
+
 Export-ModuleMember `
+    Get-MIME `
 ;
--- test.ps1
+++ test.ps1
@@ -12,47 +12,13 @@ Import-Module `
 	-Force `
 ;
 
-$fileInfo = [System.IO.FileInfo]'test\logo.jpg';
-
-$length = 0;
-[Byte[]] $buffer = $null;
-if ( [System.IO.File]::Exists( $fileInfo ) ) {
-    $fs = New-Object System.IO.FileStream (
-        $fileInfo, 
-        [System.IO.FileMode]::Open,
-        [System.IO.FileAccess]::Read,
-        [system.IO.FileShare]::Read,
-        256
-    );
-    try {
-        $buffer = New-Object Byte[] (256);
-        if ( $fs.Length -ge 256) {
-            $length = 256;
-        } else {
-            $length = $fs.Length;
-        };
-        if ( $length ) {
-            $length = $fs.Read( $buffer, 0, $length );
-        };
-    } finally {
-        $fs.Dispose();
-    };
-};
-
-[System.UInt32] $mimeType = 0;
-
-$err = [ITG.WinAPI.UrlMon]::FindMimeFromData(
-    0,
-    $fileInfo.FullName, 
-    $buffer, 
-    $length, 
-    $null, 
-    0x00000001 + 0x00000020, 
-    [ref]$mimetype, 
-    0
-);
-
-$mimeTypePtr = [System.IntPtr]$mimeType;
-$mime = [System.Runtime.InteropServices.Marshal]::PtrToStringUni( $mimeTypePtr );
-[System.Runtime.InteropServices.Marshal]::FreeCoTaskMem( $mimeTypePtr );
-$mime;
+'test\logo.jpg' `
+, 'test\logo2.jpg' `
+| % {
+    (join-path `
+        -path ( ( [System.IO.FileInfo] ( $myinvocation.mycommand.path ) ).directory ) `
+        -childPath $_ `
+    ) `
+} `
+| Get-MIME `
+;
